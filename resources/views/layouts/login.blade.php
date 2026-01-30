399d1fce8abf694076824c06b0f8c4b30b9c2cfe
git-test <>
2026-01-30 20:51:49 +0900

    こみっと

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -24,7 +24,7 @@
   <!--OGPタグ/twitterカード-->
 </head>
 
-<body class="login">
+<body>
     {{ $slot }}
     <script src="{{ asset('js/app.js') }}"></script>
     <script src="JavaScriptファイルのURL"></script>
